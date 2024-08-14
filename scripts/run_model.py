df7ec1b0de5ceb5af1ee302134e6642ce20a9200
ValenButtignol <valentinbuttignol@gmail.com>
2024-08-14 14:17:08 -0300

    Add methods to difference prompt paths

--- scripts/run_model.py
+++ scripts/run_model.py
@@ -10,14 +10,17 @@ def create_parser():
     args = parser.parse_args()
     return args
 
-def get_prompt(file_prompt, text_prompt):
-    if file_prompt is not None:
-        file = open(f"prompts/default_prompts/{file_prompt}", "r")
-        prompt = file.read()
-        return prompt
-    elif text_prompt is not None:
-        return text_prompt
+def get_default_prompt(file_prompt):
+    file = open(f"prompts/default_prompts/{file_prompt}", "r")
+    prompt = file.read()
+    return prompt
 
+def get_system_user_role_prompt(file_prompt):
+    import yaml
+    with open(f"prompts/system_user_roles_prompts/{file_prompt}", 'r') as file:
+        data = yaml.safe_load(file)
+    return data
+    
 def get_model_path(model_name):
     return f"models/{model_name}"
 
@@ -32,15 +35,16 @@ def run_model(model, prompt):
     output = model(
         prompt,
         max_tokens=1000,
-        #stop=["}\n\n"],
-        echo=True # Echo the prompt back in the output
+        echo=True
     )
     return output['choices'][0]['text']
 
-
 def execute():
     args = create_parser()
-    prompt = get_prompt(args.file_prompt, args.text_prompt)
+    if args.file_prompt:
+        prompt = get_default_prompt(args.file_prompt)
+    else:
+        prompt = args.text_prompt        
     model_path = get_model_path(args.model_name)
     print(model_path)
     model = create_model(model_path)
