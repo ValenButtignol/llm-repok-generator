fba5f8ff38ecf50d4f335bd2200d366c6f446b6e
ValenButtignol <valentinbuttignol@gmail.com>
2024-08-27 15:27:20 -0300

    Adjust model repr

--- classes/model.py
+++ classes/model.py
@@ -22,15 +22,19 @@ class Model:
         return output['choices'][0]['text'] 
     
     def __repr__(self) -> str:
-        content_width = 55
+        content_width = 80
         return f"""
-            {'-' * (content_width + 2)}
-            | Model: {self.model_path.ljust(content_width - len("Model: "))}|
-            | Temperature: {str(self.temperature).ljust(content_width - len("Temperature: "))}|
-            | Max Tokens: {str(self.max_tokens).ljust(content_width - len("Max Tokens: "))}|
-            | n_ctx: {str(self.n_ctx).ljust(content_width - len("n_ctx: "))}|
-            | Prompt: {self.prompt.get_text().ljust(content_width - len("Prompt: "))}|
-            {'-' * (content_width + 2)}
+{'-' * (content_width + 2)}
+| Model: {self.model_path.ljust(content_width - len("Model: "))}|
+| Temperature: {str(self.temperature).ljust(content_width - len("Temperature: "))}|
+| Max Tokens: {str(self.max_tokens).ljust(content_width - len("Max Tokens: "))}|
+| n_ctx: {str(self.n_ctx).ljust(content_width - len("n_ctx: "))}|
+{'-' * (content_width + 2)}
+    
+    Prompt: 
+{self.prompt.get_text()}
+    
+{'-' * (content_width + 2)}
         """
         
 # The idea should be to get the messages from prompt attribute.
