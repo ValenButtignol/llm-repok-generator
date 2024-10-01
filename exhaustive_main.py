bd169a91b0be8bebef79184c8b07a41ac3d1ee97
ValenButtignol <valentinbuttignol@gmail.com>
2024-10-01 20:49:34 -0300

    Fix bug on output file creation

--- exhaustive_main.py
+++ exhaustive_main.py
@@ -10,14 +10,15 @@ from classes.output_manager.file_output_manager import FileOutputManager
 def main():
     import os
     model_names = [model for model in os.listdir("models") if model.endswith(".gguf")]
-    prompt_paths = ["ollama_simple_prompts/" + prompt_path for prompt_path in os.listdir("prompts/ollama_simple_prompts")]
+    prompt_paths = [("ollama_simple_prompts/" + prompt_path) for prompt_path in os.listdir("prompts/ollama_simple_prompts")]
     
     for model_name in model_names:
         for prompt_path in prompt_paths:
             file_prompt = FilePrompt(prompt_path)
             print("Model: " + model_name + " Prompt: " + prompt_path)
             model = Model(f"models/{model_name}", 0.2, 300, 2048, file_prompt)
-            output_manager = FileOutputManager(model_name + "_" + prompt_path + ".txt")
+            prompt_filename = prompt_path.split("/")[-1]
+            output_manager = FileOutputManager(model_name + "_" + prompt_filename + ".txt")
             completion = model.create_completion() 
             output_manager.write(repr(model))
             output_manager.write("\n\n")
