a03207b43696fb5d68372358cad2451ced420809
Investigador <valentinbuttignol@gmail.com>
2024-10-02 15:11:25 -0300

    Make models a parameter to exhaustive_main script

--- exhaustive_main.py
+++ exhaustive_main.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import argparse
 from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parent.parent))
@@ -8,21 +10,22 @@ from classes.prompt.file_prompt import FilePrompt
 from classes.output_manager.file_output_manager import FileOutputManager
 
 def main():
-    import os
-    model_names = [model for model in os.listdir("models") if model.endswith(".gguf")]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model_name")
+    args = parser.parse_args()
+    model_name = args.model_name
     prompt_paths = [("ollama_simple_prompts/" + prompt_path) for prompt_path in os.listdir("prompts/ollama_simple_prompts")]
     
-    for model_name in model_names:
-        for prompt_path in prompt_paths:
-            file_prompt = FilePrompt(prompt_path)
-            print("Model: " + model_name + " Prompt: " + prompt_path)
-            model = Model(f"models/{model_name}", 0.2, 300, 2048, file_prompt)
-            prompt_filename = prompt_path.split("/")[-1]
-            output_manager = FileOutputManager(model_name + "_" + prompt_filename)
-            completion = model.create_completion() 
-            output_manager.write(repr(model))
-            output_manager.write("\n\n")
-            output_manager.write(completion)
+    for prompt_path in prompt_paths:
+        file_prompt = FilePrompt(prompt_path)
+        print("Model: " + model_name + " Prompt: " + prompt_path)
+        model = Model(f"models/{model_name}", 0.2, 300, 2048, file_prompt)
+        prompt_filename = prompt_path.split("/")[-1]
+        output_manager = FileOutputManager(model_name + "_" + prompt_filename)
+        completion = model.create_completion() 
+        output_manager.write(repr(model))
+        output_manager.write("\n\n")
+        output_manager.write(completion)
 
 #python3 exhaustive_main.py
 if __name__ == "__main__":
