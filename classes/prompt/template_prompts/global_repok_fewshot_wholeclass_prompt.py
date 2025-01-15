3e6e4b25cd5ae93633ea06cc848c8110d8732872
ValenButtignol <valentinbuttignol@gmail.com>
2025-01-15 17:48:01 -0300

    Add GlobalRepOkFewShotWholeClassPrompt

---
+++ classes/prompt/template_prompts/global_repok_fewshot_wholeclass_prompt.py
@@ -0,0 +1,15 @@
+from classes.prompt.json_prompt import JsonPrompt
+from classes.prompt.template_prompts.templates import SYSTEM_PROMPT_REPOK, USER_PROMPT_REPOK, CLASS_EXAMPLE_1, REPOK_EXAMPLE_1, CLASS_EXAMPLE_2, REPOK_EXAMPLE_2
+
+class GlobalRepOkFewShotWholeClassPrompt(JsonPrompt):
+    def __init__(self, classfile):
+        with open(classfile, "r") as file:
+            self.classtext = file.read()
+
+        self.data = {"messages":[]}
+        self.add_role_message("system", SYSTEM_PROMPT_REPOK)
+        self.add_user_message(USER_PROMPT_REPOK + CLASS_EXAMPLE_1)
+        self.add_assistant_message(REPOK_EXAMPLE_1)
+        self.add_user_message(USER_PROMPT_REPOK + CLASS_EXAMPLE_2)
+        self.add_assistant_message(REPOK_EXAMPLE_2)
+        self.add_user_message(USER_PROMPT_REPOK + self.classtext)
