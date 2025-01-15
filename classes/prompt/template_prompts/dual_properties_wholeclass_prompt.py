3b417358ff9910c442054cc96df17551cba1fff6
ValenButtignol <valentinbuttignol@gmail.com>
2025-01-15 18:33:18 -0300

    Change system prompts in FS with CoT prompts, and add dual prompts

---
+++ classes/prompt/template_prompts/dual_properties_wholeclass_prompt.py
@@ -0,0 +1,32 @@
+from classes.prompt.json_prompt import JsonPrompt
+from classes.prompt.template_prompts.templates import SYSTEM_PROMPT_PROP_LIST, SYSTEM_PROMPT_REPOK_AND_PROP, CLASS_EXAMPLE_1, TEXT_PROP_LIST_EXAMPLE_1, CODE_SINGLE_PROP_EXAMPLE_1, CLASS_EXAMPLE_2, TEXT_PROP_LIST_EXAMPLE_2, CODE_SINGLE_PROP_EXAMPLE_2
+
+class DualPropertiesWholeClassPrompt(JsonPrompt):
+    def __init__(self, classfile):
+        with open(classfile, "r") as file:
+            self.classtext = file.read()
+
+        self.data = {"messages":[]}
+        self.add_role_message("system", SYSTEM_PROMPT_PROP_LIST)
+        
+        self.add_user_message(CLASS_EXAMPLE_1)
+        self.add_assistant_message(TEXT_PROP_LIST_EXAMPLE_1)
+        
+        self.add_user_message(CLASS_EXAMPLE_2)
+        self.add_assistant_message(TEXT_PROP_LIST_EXAMPLE_2)
+
+        self.add_user_message(self.classtext)
+
+class DualRepOkFewShotCoTWholeClassPrompt(JsonPrompt):
+    def __init__(self, classwithprop):
+
+        self.data = {"messages":[]}
+        self.add_role_message("system", SYSTEM_PROMPT_REPOK_AND_PROP)
+        
+        self.add_user_message(CLASS_EXAMPLE_1 + TEXT_PROP_LIST_EXAMPLE_1)
+        self.add_assistant_message(CODE_SINGLE_PROP_EXAMPLE_1)
+
+        self.add_user_message(CLASS_EXAMPLE_2 + TEXT_PROP_LIST_EXAMPLE_2)
+        self.add_assistant_message(CODE_SINGLE_PROP_EXAMPLE_2)
+
+        self.add_user_message(classwithprop)
