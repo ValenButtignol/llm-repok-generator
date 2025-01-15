061d7c8346cae4de59f8d55a86bf06ac9c5d06aa
ValenButtignol <valentinbuttignol@gmail.com>
2025-01-15 17:07:54 -0300

    Fix typo in GlobalRepOkPrompt

--- classes/prompt/template_prompts/global_repok_prompt.py
+++ classes/prompt/template_prompts/global_repok_prompt.py
@@ -3,7 +3,7 @@ from classes.prompt.template_prompts.templates import SYSTEM_PROMPT_REPOK, USER_
 
 class GlobalRepOkPrompt(JsonPrompt):
     def __init__(self, classfile):
-        with open(self.classfile, "r") as file:
+        with open(classfile, "r") as file:
             self.classtext = file.read()
 
         self.add_role_message("system", SYSTEM_PROMPT_REPOK)
