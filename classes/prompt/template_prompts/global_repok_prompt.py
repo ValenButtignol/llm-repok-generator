edcac0f9973a1637800334931dea91139a08d380
ValenButtignol <valentinbuttignol@gmail.com>
2025-01-15 16:59:08 -0300

    Make GlobalRepOkPrompt to read the class from file

--- classes/prompt/template_prompts/global_repok_prompt.py
+++ classes/prompt/template_prompts/global_repok_prompt.py
@@ -2,6 +2,9 @@ from classes.prompt.json_prompt import JsonPrompt
 from classes.prompt.template_prompts.templates import SYSTEM_PROMPT_REPOK, USER_PROMPT_REPOK
 
 class GlobalRepOkPrompt(JsonPrompt):
-    def __init__(self, stringclass):
+    def __init__(self, classfile):
+        with open(self.classfile, "r") as file:
+            self.classtext = file.read()
+
         self.add_role_message("system", SYSTEM_PROMPT_REPOK)
-        self.add_user_message(USER_PROMPT_REPOK + stringclass)
+        self.add_user_message(USER_PROMPT_REPOK + self.classtext)
