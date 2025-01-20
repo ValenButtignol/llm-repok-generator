2b06125c9b41d7478fe2015e344db03cd9ab0074
ValenButtignol <valentinbuttignol@gmail.com>
2025-01-20 17:04:57 -0300

    Delete args on input_parser

--- classes/input_parser.py
+++ classes/input_parser.py
@@ -7,9 +7,6 @@ class InputParser:
     def __init__(self):
         self.parser = ArgumentParser()
         self.parser.add_argument("-mn", "--model_name", dest="model_name", help="check 'models' folder")
-        self.parser.add_argument("-tmp", "--temperature", dest="temperature", required=False, default=0.1)
-        self.parser.add_argument("-mtk", "--max_tokens", dest="max_tokens", required=False, default=1000)
-        self.parser.add_argument("-nctx", "--n_ctx", dest="n_ctx", required=False, default=2048)    
         self.parser.add_argument("-pc", "--prompt_container", dest="prompt_container", help="file or plain text containing the prompt", required=True)
         self.parser.add_argument("-pt", "--prompt_type", dest="prompt_type", help="format of the prompt", required=True)
         self.parser.add_argument("-ot", "--output_type", dest="output_type", help="type of output", required=True)
@@ -17,9 +14,6 @@ class InputParser:
     
     def parse(self):
         args = self.parser.parse_args()
-        self.temperature = args.temperature
-        self.max_tokens = args.max_tokens
-        self.n_ctx = args.n_ctx
         model_path_factory = ModelPathFactory()
         output_manager_factory = OutputManagerFactory()
         self.output_manager = output_manager_factory.create(args.output_type, args.output_container)
@@ -27,5 +21,3 @@ class InputParser:
         prompt_factory = PromptFactory()
         self.prompt = prompt_factory.create(args.prompt_type, args.prompt_container)
         
-        
-    
