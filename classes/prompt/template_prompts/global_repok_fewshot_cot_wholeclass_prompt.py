06da34826f4440086c63093357a1b30b747dce35
ValenButtignol <valentinbuttignol@gmail.com>
2025-01-20 15:40:28 -0300

    Modify and delete useless string templates

--- classes/prompt/template_prompts/global_repok_fewshot_cot_wholeclass_prompt.py
+++ classes/prompt/template_prompts/global_repok_fewshot_cot_wholeclass_prompt.py
@@ -1,5 +1,5 @@
 from classes.prompt.json_prompt import JsonPrompt
-from classes.prompt.template_prompts.templates import SYSTEM_PROMPT_REPOK, SYSTEM_PROMPT_COT_REPOK, USER_PROMPT_PROP_LIST, USER_PROMPT_CHAIN_OF_THOUGT, CLASS_EXAMPLE_1, TEXT_PROP_LIST_EXAMPLE_1, REPOK_EXAMPLE_1, CLASS_EXAMPLE_2, TEXT_PROP_LIST_EXAMPLE_2, REPOK_EXAMPLE_2
+from classes.prompt.template_prompts.templates import SYSTEM_PROMPT_REPOK, USER_PROMPT_PROP_LIST, USER_PROMPT_CHAIN_OF_THOUGT, CLASS_EXAMPLE_1, TEXT_PROP_LIST_EXAMPLE_1, REPOK_EXAMPLE_1, CLASS_EXAMPLE_2, TEXT_PROP_LIST_EXAMPLE_2, REPOK_EXAMPLE_2
 
 class GlobalRepOkFewShotCoTWholeClassPrompt(JsonPrompt):
     def __init__(self, classfile):
@@ -7,7 +7,7 @@ class GlobalRepOkFewShotCoTWholeClassPrompt(JsonPrompt):
             self.classtext = file.read()
 
         self.data = {"messages":[]}
-        self.add_role_message("system", SYSTEM_PROMPT_COT_REPOK)
+        self.add_role_message("system", SYSTEM_PROMPT_REPOK)
         self.add_user_message(USER_PROMPT_PROP_LIST + CLASS_EXAMPLE_1)
         self.add_assistant_message(TEXT_PROP_LIST_EXAMPLE_1)
         self.add_user_message(USER_PROMPT_CHAIN_OF_THOUGT)
