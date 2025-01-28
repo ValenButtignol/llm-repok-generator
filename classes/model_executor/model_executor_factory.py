6b39acb44f94c6528b16a8a21489751a3cbf30b2
ValenButtignol <valentinbuttignol@gmail.com>
2025-01-28 15:43:59 -0300

    Implement model_executor factory and simple_model_executor

--- classes/model_executor/model_executor_factory.py
+++ classes/model_executor/model_executor_factory.py
@@ -1,4 +1,20 @@
+from classes.model_executor.dual_partsofclass_model_executor import DualPartsOfClassModelExecutor
+from classes.model_executor.dual_wholeclass_model_executor import DualWholeClassModelExecutor
+from classes.model_executor.simple_model_executor import SimpleModelExecutor
+
 class ModelExecutorFactory:
     
     def create(self, prompt_type):
-        raise NotImplementedError
+        if prompt_type == "global":
+            return SimpleModelExecutor()
+        elif prompt_type == "fs-w":
+            return SimpleModelExecutor()
+        elif prompt_type == "fs-p":
+            return SimpleModelExecutor()
+        elif prompt_type == "dual-w":
+            return DualWholeClassModelExecutor()
+        elif prompt_type == "dual-p":
+            return DualPartsOfClassModelExecutor()
+    
+        else:
+            raise Exception("Invalid prompt type: " + prompt_type + "\n")
--- classes/model_executor/simple_model_executor.py
+++ classes/model_executor/simple_model_executor.py
@@ -1,11 +1,6 @@
 from classes.model_executor.model_executor_interface import ModelExecutorInterface
 
 class SimpleModelExecutor(ModelExecutorInterface):
-    def __init__(self):
-        raise NotImplementedError
 
     def execute(self, model):
-        raise NotImplementedError
-    
-    def write_output(self, completion):
-        raise NotImplementedError
+        return model.create_chat_completion()
