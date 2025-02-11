80114db4e77f1dbd23b41f62e9e5faecef6a785c
ValenButtignol <valentinbuttignol@gmail.com>
2025-02-11 17:21:49 -0300

    Restore property enumeration

--- classes/repok_parser/dual_prop_repok_parser.py
+++ classes/repok_parser/dual_prop_repok_parser.py
@@ -8,6 +8,7 @@ class DualPropRepOkParser(RepOkParserInterface):
 
     def parse(self) -> str:
         code_properties = self._parse_code_properties()
+        code_properties = self._enumerate_properties(code_properties)
         return self._build_repOk_classes(code_properties)
     
     def _parse_code_properties(self):
@@ -29,6 +30,11 @@ class DualPropRepOkParser(RepOkParserInterface):
                 current_property += TAB + line + "\n"
 
         return properties
+
+    def _enumerate_properties(self, code_properties : list):
+        for i, prop in enumerate(code_properties):
+            enumerated_prop = prop.replace(PROPERTY_METHOD_NAME, PROPERTY_METHOD_NAME + str(i+1))
+            code_properties[i] = enumerated_prop    
 
     def _build_repOk_classes(self, prop_list) -> str:
         classes = []
