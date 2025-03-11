223e414d1ccb439a40d62873ff3aa077a0f806b5
ValenButtignol <valentinbuttignol@gmail.com>
2025-03-11 12:34:24 -0300

    Refactor RepOKParser into a single class

--- classes/repok_parser/repok_parser_interface.py
+++
@@ -1,8 +0,0 @@
-class RepOkParserInterface:
-
-    def set_repOk_completion(self, repOk_completion : str):
-        self.repOk_completion = repOk_completion
-
-    def parse(self) -> str:
-        raise NotImplementedError
-    
